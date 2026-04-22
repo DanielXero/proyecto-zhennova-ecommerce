5fdff849874160b726cc51f417396682e7bbe1f5
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2026-04-22 10:49:10 -0300

    modificando estilos

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
@@ -19,6 +9,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
