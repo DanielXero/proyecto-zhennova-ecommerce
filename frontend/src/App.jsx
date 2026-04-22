7c17e01bb41d8c4aab819ccc9864933255f6ac86
Julian Edgardo Mieres <julianmierez@gmail.com>
2026-04-22 01:56:11 -0300

    configurar rutas principales y conectar componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -119,3 +119,31 @@ function App() {
 }
 
 export default App
+
+import { Header } from './components/Header'
+import { Footer } from './components/Footer'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import { ProductList } from './components/ProductList'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/productos" element={<ProductList />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
+
