7cf9badb63ee0f8bc3ea74cd055b171d69a270a7
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2026-04-28 22:03:33 -0300

    integrar adminProducts en el store global

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,10 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit'
 import usersReducer from './usersSlice'
 import productsReducer from './productsSlice'
+import adminProductsReducer from './adminProductsSlice'
+
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
     products: productsReducer,
+    adminProducts: adminProductsReducer,
   },
 })
