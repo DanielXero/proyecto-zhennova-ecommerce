b0d55703667a635eb49387784e6353a9b179cb71
Gustavo Sosa <117545957+DanielXero@users.noreply.github.com>
2026-06-08 14:26:19 -0300

    Mejora del panel de administración con paginación, vista previa de imágenes y notificaciones toast globales

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,10 +5,12 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'react-toastify/dist/ReactToastify.css' 
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
+import { ToastContainer } from 'react-toastify'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -16,6 +18,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
         <App />
+        <ToastContainer 
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
